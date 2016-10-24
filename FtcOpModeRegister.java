cbc0c50064218916c9eed559b0e8650904d6d962
karen1927 <ktolentino@cps.edu>
2016-10-24 17:28:38 -0500

    Add files via upload

---
+++ FtcOpModeRegister.java
@@ -0,0 +1,101 @@
+/* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.AnnotatedOpModeRegistrar;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
+import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.ConceptNullOp;
+
+/**
+ * {@link FtcOpModeRegister} is responsible for registering opmodes for use in an FTC game.
+ * @see #register(OpModeManager)
+ */
+public class FtcOpModeRegister implements OpModeRegister {
+
+    /**
+     * {@link #register(OpModeManager)} is called by the SDK game in order to register
+     * OpMode classes or instances that will participate in an FTC game.
+     * <p>
+     * There are two mechanisms by which an OpMode may be registered.
+     * <p>
+     * 1) The preferred method is by means of class annotations in the OpMode itself.
+     * See, for example the class annotations in {@link ConceptNullOp}.
+     * <p>
+     * 2) The other, retired,  method is to modify this {@link #register(OpModeManager)}
+     * method to include explicit calls to OpModeManager.register().
+     * This method of modifying this file directly is discouraged, as it
+     * makes updates to the SDK harder to integrate into your code.
+     *
+     * @param manager the object which contains methods for carrying out OpMode registrations
+     * @see com.qualcomm.robotcore.eventloop.opmode.TeleOp
+     * @see com.qualcomm.robotcore.eventloop.opmode.Autonomous
+     */
+    public void register(OpModeManager manager) {
+
+        /**
+         * Register OpModes implemented in the Blocks visual programming language.
+         */
+        BlocksOpMode.registerAll(manager);
+
+        /**
+         * Register OpModes that use the annotation-based registration mechanism.
+         */
+        AnnotatedOpModeRegistrar.register(manager);
+
+        /**
+         * Any manual OpMode class registrations should go here.
+         */
+        manager.register("run first", JoystickWorks.class);
+    }
+}
+    /**
+     * This file contains an example of an iterative (Non-Linear) "OpMode".
+     * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+     * The names of OpModes appear on the menu of the FTC Driver Station.
+     * When an selection is made from the menu, the corresponding OpMode
+     * class is instantiated on the Robot Controller and executed.
+     *
+     * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+     * It includes all the skeletal structure that all iterative OpModes contain.
+     *
+     * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+     * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+     */
+
---
+++ JoystickWorks.java
@@ -0,0 +1,138 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.robot.Robot;
+import com.qualcomm.robotcore.util.Range;
+@TeleOp(name="Template: Iterative OpMode", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
+@Disabled
+public  class JoystickWorks extends OpMode {
+    /* Public OpMode members. */
+    public DcMotor motor1;
+    public DcMotor motor2;
+        /* Constructor */
+
+
+    /* Initialize standard Hardware interfaces */
+    @Override
+    public void init() {
+        // save reference to HW Map
+
+
+        motor1=hardwareMap.dcMotor.get("motor_1");
+        motor2=hardwareMap.dcMotor.get("motor_2");
+
+        motor1.setPower(.5);
+        motor2.setPower(.5);
+
+
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        motor2.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+
+
+
+
+
+
+
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+
+    // write the values to the motors
+
+
+
+    // leadScrew.setPower(powerScrew);
+                /*
+                 * Code to run ONCE when the driver hits PLAY
+                 */
+    @Override
+    public void start() {
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+
+        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+        // leftMotor.setPower(-gamepad1.left_stick_y);
+        // rightMotor.setPower(-gamepad1.right_stick_y);
+
+        float left = -gamepad1.left_stick_y;//gets information from joystick
+        float right = -gamepad1.right_stick_y;
+        right = Range.clip(right, -1, 1);//clips values into section
+        left = Range.clip(left, -1, 1);
+
+        // scale the joystick value to make it easier to control
+        // the robot more precisely at slower speeds.
+
+        right = (float)scaleInput(right);
+        left =  (float)scaleInput(left);
+        left = (float) (left*.9);
+        right = (float) (right*.9);
+
+        motor1.setPower(right);
+        motor2.setPower(left);
+    }
+
+                /*
+                 * Code to run ONCE after the driver hits STOP
+                 */
+
+
+
+    @Override
+    public void stop() {
+    }
+
+
+
+
+    double scaleInput(double dVal)  {
+        double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
+                0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
+
+        // get the corresponding index for the scaleInput array.
+        int index = (int) (dVal * 16.0);
+
+        // index should be positive.
+        if (index < 0) {
+            index = -index;
+        }
+
+        // index cannot exceed size of array minus 1.
+        if (index > 16) {
+            index = 16;
+        }
+
+        // get value from the array.
+        double dScale = 0.0;
+        if (dVal < 0) {
+            dScale = -scaleArray[index];
+        } else {
+            dScale = scaleArray[index];
+        }
+
+        // return scaled value.
+        return dScale;
+    }
+
+}
+
