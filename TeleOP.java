9169f882d3a7a7654845ff502036dc797f3ebe17
karen1927 <ktolentino@cps.edu>
2016-11-07 16:44:52 -0600

    Add files via upload

--- TeleOP.java
+++ TeleOP.java
@@ -1,4 +1,5 @@
 
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -10,94 +11,93 @@ import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
-public  class TeleOp extends OpMode {
+public class Tele extends OpMode {
     /* Public OpMode members. */
-    public DcMotor TeleOp1;
-        /* Constructor */
+
+    public DcMotor motor1;
+    public DcMotor motor2;
+    public DcMotor leadscrew1;
+        /* Constructor */  /* Constructor */
 
 
 
-    /* Initialize standard Hardware interfaces */
     @Override
     public void init() {
-        // save reference to HW Map
-
-
-        TeleOp1=hardwareMap.dcMotor.get("TeleOp_1");
-
-
-        TeleOp1.setPower(0);
-
-
-        TeleOp1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
- ]
 
 
 
 
 
+        @Override
+        public void init(){
+            // save reference to HW Map
+
+
+
+        }
 
 
 
 
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
 
+        @Override
+        public void init_loop() {
+            motor1=hardwareMap.dcMotor.get("motor_1");
+            motor2=hardwareMap.dcMotor.get("motor_2");
+            leadscrew1=hardwareMap.dcMotor.get("leadscrew_1");
 
-    // write the values to the motors
+            motor1.setPower(0);
+            motor2.setPower(0);
+            leadscrew1.setPower(0);
 
+            motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            leadscrew1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
-    // leadScrew.setPower(powerScrew);
-                /*
-                 * Code to run ONCE when the driver hits PLAY
-                 */
-    @Override
-    public void start() {
-
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
-    @Override
-    public void loop() {
-
-        // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-        // leftMotor.setPower(-gamepad1.left_stick_y);
-        // rightMotor.setPower(-gamepad1.right_stick_y);
-
-        float screwpower = -gamepad1.left_stick_y;
-        screwpower = Range.clip(screwpower, -1, 1);//clips values into section
-
-
-        // scale the joystick value to make it easier to control
-        // the robot more precisely at slower speeds.
-
-        screwpower = (float)scaleInput(screwpower);
-
-        screwpower = (float) (screwpower*.9);
-
-        TeleOp1.setPower(screwpower);
-    }
-
-                /*
-                 * Code to run ONCE after the driver hits STOP
-                 */
-
-
-
-    @Override
-    public void stop() {
-    }
+            motor2.setDirection(DcMotor.Direction.REVERSE);
 
 
 
 
+
+
+
+        }
+
+
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void loop() {
+
+            // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
+            // leftMotor.setPower(-gamepad1.left_stick_y);
+            // rightMotor.setPower(-gamepad1.right_stick_y);
+            float left = -gamepad2.left_stick_y;
+            float left = -gamepad1.left_stick_y;//gets information from joystick
+            float right = -gamepad1.right_stick_y;
+            float right = -gamepad2.right_stick_y;
+            right = Range.clip(right, -1, 1);//clips values into section
+            left = Range.clip(left, 1, -1);
+
+            // scale the joystick value to make it easier to control
+            // the robot more precisely at slower speeds.
+
+            right = (float)scaleInput(right);
+            left =  (float)scaleInput(left);
+            left = (float) (left*.9);
+            right = (float) (right*.9);
+
+            motor1.setPower(right); //Motor one goes counter clockwise//
+            motor2.setPower(left);
+        }
+        @Override
+        public void stop() {
+        }
     double scaleInput(double dVal)  {
         double[] scaleArray = { 0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00 };
