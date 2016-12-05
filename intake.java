2f9eb3fc522c2d0d7180652a5895908b8ceb95c3
karen1927 <ktolentino@cps.edu>
2016-12-05 17:23:17 -0600

    Add files via upload

--- intake.java
+++ intake.java
@@ -9,6 +9,8 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.robot.Robot;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
 
  @TeleOp(name="Intake", group="Iterative Opmode")  // @Autonomous(...) is the other common choice
  @Disabled
@@ -17,10 +19,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
      private ElapsedTime runtime = new ElapsedTime();
      public DcMotor  leftintake;
      public DcMotor rightintake;
+     public   Servo serveleft;//Declares servo hooks on front for bars
+     Servo serveright;
      int count = 0;
      double currentTime = 0;
      ElapsedTime time;
-
+     ServoController screwController;//Declares servo controller
+   //screwPosition2 = .6;
+   //screwPosition1 = .4;
      /* Constructor */
      /* Initialize standard Hardware interfaces */
      @Override
@@ -30,13 +36,22 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
          leftintake = hardwareMap.dcMotor.get("intake_1");
          rightintake = hardwareMap.dcMotor.get("intake_2");
+         serveleft = hardwareMap.servo.get("servo_1");
+         serveright = hardwareMap.servo.get("servo_2");
 
          rightintake.setPower(0);
          leftintake.setPower(0);
 
+
          rightintake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
          leftintake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+         screwController = hardwareMap.servoController.get("servoController");//creats servoControler
+         screwController.pwmEnable();
+         double  serveleft;//Declares values for initial screw position
+         double  serveright;
+         // amount to change the claw servo position by
      }
+
 
 
      /*
@@ -98,35 +113,77 @@ import com.qualcomm.robotcore.util.ElapsedTime;
              leftintake.setPower(0);
              rightintake.setPower(0);
          }
+             else {
+                 leftintake.setPower(0);
+                 rightintake.setPower(0);
+         }
+     /*
+                 * Code to run ONCE after the driver hits STOP
+                 */
+         if (gamepad1.a)
+         {
+             serveleftt.setPosition(.45);
+             serveright.setPosition(.45);
+         }
+         serveleftt.setPosition(0);
+         serveright.setPosition(0);
+         else if(gamepad1.b)
+{
+    serveleftt.setPosition(-.45);
+    serveright.setPosition(-.45);
+}
+         serveleftt.setPosition(0);
+         serveright.setPosition(0);
+
+         else if (gamepad1.left_bumper)
+         {
+             runtime.reset();//time is 0
+             while(currentTime< .01) //while current time < set period of time
+             {
+
+                 leftintake.setPower(-1);
+                 rightintake.setPower(-1);
+
+
+             }
+             leftintake.setPower(0);
+             rightintake.setPower(0);
+         }
          else {
              leftintake.setPower(0);
              rightintake.setPower(0);
          }
-   /*  if (gamepad1.a)
-     {
-         intake2.setPower(1);
-     }
+//
 
-     else if (gamepad1.b)
-     {
-         intake2.setPower(-1);
+      // if (gamepad1.a)
+  //     { serveleft.setPower(1);
+    //       serveleft.setPower(1);
+//           }//will do this action for set period of time
+         //  leftintake.setPower(0);
+         //  rightintake.setPower(0);
+       //}
+      // else if (gamepad1.b)
+     //  {
+           //runtime.reset();//time is 0
+      //     while(currentTime< .01) //while current time < set period of time
+       //    {
 
-     }
-     else
-     {
-         intake2.setPower(0);
+         //      leftintake.setPower(-1);
+           //    rightintake.setPower(-1);
 
-     }
 
-         */       /*
-                 * Code to run ONCE after the driver hits STOP
-                 */
-
-     }
+            //
+           //leftintake.setPower(0);
+          // rightintake.setPower(0);
+      // }
+       //else {
+         //  leftintake.setPower(0);
+       //    rightintake.setPower(0);
+      // }//
+   //}
          @Override
          public void stop() {
          }
 
 
      }
-
